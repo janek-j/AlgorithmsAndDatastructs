c0c4f45bf317757fcba238fb723ccc65737c05f7
janek <jan.jochymczyk@gmail.com>
2023-11-21 13:00:19 +0100

    2 tasks commit.

---
+++ Task1/zad1.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+
+//Jan Jochymczyk
+
+using namespace std;
+
+typedef int elementtype, position ;
+const int maxlength = 10;
+struct List {
+    elementtype elements[maxlength];
+    int last;           //indeks ostatniego elementu listy
+};
+
+position END(List l) {
+    return l.last + 1;
+}
+
+position First(List l) {
+    return 0;
+}
+
+position Next(position p,List l) {
+    return p + 1;
+}
+
+
+position Previous(position p,List l) {
+    return p - 1;
+}
+
+position Locate(elementtype x, List l) {
+    //zwraca pozycje x w l, gdy nie ma x w l zwraca end(l)
+    for(int i = 0; i <= l.last; ++i) {
+        if(l.elements[i] == x) {
+            return i;
+        }
+    }
+    return END(l);
+}
+
+elementtype Retrieve(position p, List l) {
+    return l.elements[p];
+}
+
+bool Insert(int x, position p, List &l) {
+    if (p < 0 || p > l.last + 1 || l.last == maxlength - 1) {
+        return false; // warunek poprawnosci pozycji.
+    }
+
+    for (int i = l.last; i >= p; --i) {
+        l.elements[i + 1] = l.elements[i];
+    }
+    l.elements[p] = x;
+    ++l.last; //1 el. wiecej, wiec index ostatniego elementu zwiekszam o 1.
+
+    return true;
+}
+
+bool Delete(position p, List &l) {
+    if (p < 0 || p > l.last) {
+        return false; // warunek blednego podania indeksu.
+    }
+    for (int i = p; i < l.last; ++i) {
+        l.elements[i] = l.elements[i + 1];
+    }
+
+    --l.last;
+    return true;
+}
+
+void print(List l)
+{
+    position i=First(l);
+    while (i!=END(l))
+    {
+        printf ("  %d,", Retrieve(i,l));
+        i=Next(i,l);
+    }
+    printf("\n");
+
+}
+
+void initiateTask1() {
+    List l;
+    l.last=-1;
+    Insert(100,First(l),l);
+    print(l);
+
+    for (int i=0; i<3;i++)
+        Insert(i, First(l),l);
+    print(l);
+
+    Insert(20, Previous(END(l), l) ,l);
+    print(l);
+    Delete(Locate(20,l), l);
+    print(l);
+}
+
+int main(int args, char* argv[]) {
+    initiateTask1();
+    return 0;
+}
---
+++ Task2/zad2.cpp
@@ -0,0 +1,179 @@
+#include <iostream>
+
+// linked list implementation.
+
+typedef int  elementtype;
+
+struct celltype {
+    elementtype element;
+    celltype * next;
+};
+
+typedef celltype  * position;
+
+class Lista
+{
+protected :
+    position l; //wskaźnik do głowy listy
+public:
+    Lista();    //konstruktor
+    ~Lista();  //destruktor
+    void Insert(elementtype x, position p);
+    void Delete(position p);
+    elementtype Retrieve(position p);
+    position Locate(elementtype x);
+    position First();
+    position Next(position p);
+    position Previous(position p);
+    position END();
+};
+
+Lista::Lista() {
+    l = new celltype();
+    l -> next = nullptr;
+}
+
+Lista::~Lista() {
+    position current = l;
+    position nextNode;
+
+    // Traverse the list and delete each node
+    while (current != nullptr) {
+        nextNode = current->next;
+        delete current;
+        current = nextNode;
+    }
+
+    // Make sure to set the head pointer to null after deleting all nodes
+    l = nullptr;
+}
+
+position Lista::First() {
+    return l;
+}
+
+position Lista::END() {
+    position tmp;
+    tmp = l;
+    while(tmp -> next != nullptr) {
+        tmp = tmp -> next;
+    }
+    return tmp;
+}
+
+position Lista::Next(position p) {
+    if(p != nullptr) {
+        return p -> next;
+    }
+    return nullptr;
+}
+
+position Lista::Previous(position p) {
+    if (p != nullptr && p != l) {
+        position current = l;
+        while (current != nullptr && current -> next != nullptr && current -> next != p) {
+            current = current -> next;
+        }
+        return current;
+    } else {
+        // Handle error or return a special value to indicate an error
+        return nullptr;
+    }
+}
+
+position Lista::Locate(elementtype x) {
+    position current = l;
+    while(current != nullptr && current -> next != nullptr) {
+        if(current -> next -> element == x) {
+            return current;
+        }
+        current = current -> next;
+    }
+}
+
+void Lista::Insert(elementtype x, position p) {
+    position newNode = new celltype; // Create a new node
+    newNode->element = x;
+    newNode->next = p->next;
+    p->next = newNode;
+}
+
+void Lista::Delete(position p) {
+    if (p != nullptr && p->next != nullptr) {
+        position temp = p->next;
+        p->next = p->next->next;
+        delete temp;
+    }
+}
+
+elementtype Lista::Retrieve(position p) {
+    if (p != nullptr && p->next != nullptr) {
+        return p->next->element;
+    } else {
+        return -1; // For example, -1 could indicate an error
+    }
+}
+
+void initaiteTask2() {
+    Lista lista;
+
+    // Wstaw 5 na początek
+    lista.Insert(5, lista.First());
+
+    // Wypisz pierwszy element
+    std::cout << "Pierwszy element: " << lista.Retrieve(lista.First()) << std::endl;
+
+    // Wstaw 2 na początek
+    lista.Insert(2, lista.First());
+
+    // Wypisz pierwszy element
+    std::cout << "Pierwszy element: " << lista.Retrieve(lista.First()) << std::endl;
+
+    // Wypisz drugi element
+    std::cout << "Drugi element: " << lista.Retrieve(lista.Next(lista.First())) << std::endl;
+
+    // Wstaw 1 za pierwszym elementem
+    lista.Insert(1, lista.Next(lista.First()));
+
+    // Wypisz listę
+    position current = lista.First();
+    while (current != nullptr && current->next != nullptr) {
+        std::cout << current->next->element << " ";
+        current = lista.Next(current);
+    }
+    std::cout << std::endl;
+
+    // Wstaw 100 na koniec listy
+    lista.Insert(100, lista.END());
+
+    // Wypisz listę
+    current = lista.First();
+    while (current != nullptr && current->next != nullptr) {
+        std::cout << current->next->element << " ";
+        current = lista.Next(current);
+    }
+    std::cout << std::endl;
+
+    // Usuń element przed wartością 100
+    position positionBefore100 = lista.Locate(100);
+    if (positionBefore100 != nullptr) {
+        lista.Delete(positionBefore100);
+    }
+
+    // Wypisz listę
+    current = lista.First();
+    while (current != nullptr && current->next != nullptr) {
+        std::cout << current->next->element << " ";
+        current = lista.Next(current);
+    }
+    std::cout << std::endl;
+
+    // Koniec listy
+    position endPosition = lista.END();
+    std::cout << "Koniec listy: " << lista.Retrieve(endPosition) << std::endl;
+}
+
+int main(int args, char* argv[]) {
+   initaiteTask2();
+}
+
